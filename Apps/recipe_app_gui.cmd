d4d84f05dbd69b7a47c2def2b83d39f9c0ec1909
OdinWattz <145436065+OdinWattz@users.noreply.github.com>
2024-10-11 11:55:09 +0200

    Werkend en .exe

---
+++ Apps/recipe_app_gui.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'recipe_app_gui.exe' with Python DLL being found.
+set PATH=C:\Users\odinw\AppData\Local\Programs\Python\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Users\odinw\AppData\Local\Programs\Python\Python312
+set NUITKA_PYTHONPATH=C:\Users\odinw\OneDrive - ROC Alfa-college\Schooljaar 24-25\Python Project Recepten beheerder;C:\Users\odinw\AppData\Local\Programs\Python\Python312\DLLs;C:\Users\odinw\AppData\Local\Programs\Python\Python312\Lib;C:\Users\odinw\AppData\Local\Programs\Python\Python312;C:\Users\odinw\AppData\Roaming\Python\Python312\site-packages;C:\Users\odinw\AppData\Local\Programs\Python\Python312\Lib\site-packages;C:\Users\odinw\AppData\Local\Programs\Python\Python312\Lib\site-packages\setuptools\_vendor
+"%~dp0recipe_app_gui.exe" %*
